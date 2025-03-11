7553c0609f04efd1d41070e12d8eaba0fe206928
Dereck <dexcka3@gmail.com>
2025-03-11 02:11:35 -0500

    Se agregó maquetado con flex y grid

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,43 +1,57 @@
 import React, { useState } from 'react'
 
 function Weather({ weather, image }) {
-//    console.log(weather);
+  //    console.log(weather);
 
-    const [isFah, setIsFah] = useState(false)
+  const [isFah, setIsFah] = useState(false)
 
-    const temperature = isFah ? `${(weather.temp * 9 / 5) + 32} °F` : `${weather.temp} °C`
+  const temperature = isFah ? `${(weather.temp * 9 / 5) + 32} °F` : `${weather.temp} °C`
   return (
     <div
-      className="container"
+      className="grid-container"
       style={{
         backgroundImage: `url(${image})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-    
+
       }}
     >
-        <div className='title'>
-        <h1>Weather app</h1>
-        </div>
+      <div className='item header'>
+        <span className='title'>
+          <h1>Weather app</h1>
+        </span>
         <p>{weather.city}, {weather.country}</p>
-        <div>
-            {/* <img src="" alt="" /> */}
-            <div className='information'>
-                <h2>{weather.description}</h2>
-                <ul>
-                    <li>Wind Speed {weather.speed}m/s</li>
-                    <li>Clouds {weather.clouds}%</li>
-                    <li>Pressure {weather.pressure}hPa</li>
-                </ul>
-            </div>
-        </div>
-        <div>
-            <h3>{temperature}</h3>
-            <button onClick={() => setIsFah(!isFah)}>
-                Change to °F {isFah ? 'Celsius': 'Fahrenheit'}
-            </button>
-        </div>
-        </div>
+      </div>
+
+
+      <div className='item icon'>
+        {/* <img src="" alt="" /> */}
+        <h2>icon</h2>
+      </div>
+
+      <div className='item middle'>
+        <h2>{weather.description}</h2>
+        <ul>
+          <li className="weather-info">
+            Wind Speed: <span className="value">{weather.speed}m/s</span>
+          </li>
+          <li className="weather-info">
+            Clouds: <span className="value">{weather.clouds}%</span>
+          </li>
+          <li className="weather-info">
+            Pressure: <span className="value">{weather.pressure}hPa</span>
+          </li>
+        </ul>
+      </div>
+
+
+      <div className='item footer'>
+        <h3>{temperature}</h3>
+        <button onClick={() => setIsFah(!isFah)}>
+          Change to °F {isFah ? 'Celsius' : 'Fahrenheit'}
+        </button>
+      </div>
+    </div>
   )
 }
 
